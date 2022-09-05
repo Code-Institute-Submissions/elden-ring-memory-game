3e96abac537d1beb2b8fcac53bf53f0519700bc0
Sebastiano Ballotta <sebeballotta@outlook.com>
2022-09-05 19:28:28 +0200

    Initialized the array of figures and the global selection of elements.

---
+++ assets/js/script.js
@@ -0,0 +1,20 @@
+/*THE CARDS WILL BE GENERATED FROM AN ARRAY OF FIGURES
+*/
+
+const cardFigures = [];
+
+/*THE ELEMENTS WILL BE SELECTED GLOBALLY IN ORDER TO BE RECYCLED*/
+
+const gameBoard = document.getElementById('game-board');
+const gameArea = document.getElementById('game-area');
+
+const guess = document.getElementById('guess');
+const fail = document.getElementById('fail');
+
+const resetButton = document.getElementById('reset');
+const startButton = document.getElementById('start');
+
+const showRulesButton = document.getElementById('show-rules');
+const hideRulesButton = document.getElementById('hide-rules');
+
+
